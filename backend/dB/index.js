b3ad8a4abfd70be4d44faba5408cafea61be5db9
Sreelakshmi Prem <sreelakshmiprem27@gmail.com>
2023-05-26 16:56:06 +0530

    first commit

--- backend/dB/index.js
+++ backend/dB/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://sreelakshmi24:sreelakshmi24@sreelakshmiprem24.l4einlx.mongodb.net/')
+mongoose.connect('mongodb+srv://sreelakshmi24:sreelakshmi24@sreelakshmiprem24.l4einlx.mongodb.net/product')
 .then(() =>
 {
     console.log('Connected to MongoDB');
